acc8e4cb3530f3e0fd8ea7d7f836b27aed39885a
tpquy <nagaky4@gmail.com>
2019-09-11 17:32:09 +0700

    initial project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BarChart from './components/BarChart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row mt-5">
+          <BarChart data={[49, 104, 159, 323, 513, 256, 173, 345, 513, 642, 334]} size={[500, 500]} />
+        </div>
+      </div>
     </div>
   );
 }
